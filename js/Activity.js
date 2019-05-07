2fc17d39d8b46273f9d7885c20e24a455897fc18
Josh Rogers <joshromanrogers@gmail.com>
2019-05-07 20:47:20 +0200

    Updates

--- js/Activity.js
+++
@@ -1,9 +0,0 @@
-export default class Activity {
-	constructor(name, media, link, categories, font) {
-		this.name = name;
-		this.media = media;
-		this.link = link;
-		this.categories = categories;
-		this.font = font;
-	}
-}
